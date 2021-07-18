3865ced9f7a7f28fd6c5d26e8220e019cb7c6bee
Robert <rshredow@gmail.com>
2021-07-17 19:02:18 -0700

    add update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div>
                 <h1>Welcome to reveries.lab</h1>
-                <p>This is my website I made with the Laravel framework in PHP.</p>
+                <p>This is my website I made with the Laravel framework in PHP. Update.</p>
             </div>
         </div>
     </div>
